69c24c7e34b6d5bea42cc7eb6331929681025031
Karol Ankutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2022-04-24 21:59:16 +0200

    Add cPostion class
    Add new auximilary class - cPosition which modelling position movable objects on game board.

---
+++ Sokoban/main/cposition.hpp
@@ -0,0 +1,25 @@
+/********** BEGIN_OF_FILE **********/
+/*
+ * author: Karol Ankutowicz
+ * file: cposition.hpp
+ */
+
+#pragma once
+
+class cPosition
+{
+    /********** PUBLIC: BEGIN **********/
+
+public:
+
+    /********** PUBLIC: END **********/
+
+    /********** PRIVATE: BEGIN **********/
+
+private:
+
+    /********** PRIVATE: END **********/
+};
+
+/* cposition.hpp */
+/********** END_OF_FILE **********/
