88cc0fc66060528fcfd9dfc6b35fd88f48535781
Karol Ankutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2022-04-24 22:32:53 +0200

    Constructors cPosition class
    Add definitions of 2 constructors in cPosition class.

--- Sokoban/main/cposition.cpp
+++ Sokoban/main/cposition.cpp
@@ -1,1 +1,32 @@
+/********** BEGIN_OF_FILE **********/
+/*
+ * author: Karol Ankutowicz
+ * file: cposition.cpp
+ */
+
 #include "cposition.hpp"
+
+/********** PUBLIC: BEGIN **********/
+
+/*
+ * cPosition()
+ */
+cPosition::cPosition()
+{
+	xPosition = 0; // ustanowienie wartosci wspolrzednej horyzontalnej
+	yPosition = 0; // ustanowienie wartosci wspolrzednej wertykalnej
+}
+
+/*
+ * cPosition(int xPos, int yPos)
+ */
+cPosition::cPosition(int xPos, int yPos)
+{
+	xPosition = xPos; // ustanowienie wartosci wspolrzednej horyzontalnej
+	yPosition = yPos; // ustanowienie wartosci wspolrzednej wertykalnej
+}
+
+/********** PUBLIC: END **********/
+
+/* cposition.cpp */
+/********** END_OF_FILE **********/
