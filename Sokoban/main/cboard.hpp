33068b931d4dab21340a835dd00f472e58cc4aa9
Karol Ankutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2022-05-28 00:56:50 +0200

    New fields in cBoard
    Add 2 (vHigh, vWide) new fields and getters and setters in cBoard class.

--- Sokoban/main/cboard.hpp
+++ Sokoban/main/cboard.hpp
@@ -26,6 +26,56 @@ public:
      */
     cBoard();
 
+    /*
+     * unsigned int getHigh() - metoda zwracajaca wartosc pola vHigh.
+     * PRE:
+     * - brak;
+     * POST:
+     * - zwrocenie wartosci pola vHigh.
+     */
+    inline unsigned int getHigh()
+    {
+        return vHigh;
+    }
+
+    /*
+     * unsigned int getWide() - metoda zwracajaca wartosc pola vWide.
+     * PRE:
+     * - brak;
+     * POST:
+     * - zwrocenie wartosci pola vWide.
+     */
+    inline unsigned int  getWide()
+    {
+        return vWide;
+    }
+
+    /*
+     * void setHigh(const unsigned int aHigh) - metoda ustanawiajaca
+     * wartosc pola vHigh.
+     * PRE:
+     * - podanie wysokosci planszy (typ: unsigned int);
+     * POST:
+     * - brak.
+     */
+    inline void setHigh(const unsigned int aHigh)
+    {
+        vHigh = aHigh;
+    }
+
+    /*
+     * void setHigh(const unsigned int aWide) - metoda ustanawiajaca
+     * wartosc pola aWide.
+     * PRE:
+     * - podanie szerokosci planszy (typ: unsigned int);
+     * POST:
+     * - brak.
+     */
+    inline void setWide(const unsigned int aWide)
+    {
+        vWide = aWide;
+    }
+
     /********** PUBLIC: END **********/
 
     /********** PRIVATE: BEGIN **********/
@@ -36,6 +86,16 @@ private:
      * cField* tabGameBoard - wskaznik do tablicy pol.
      */
     cField* tabGameBoard;
+
+    /*
+     * unsigned int vHigh - wysokosc planszy.
+     */
+    unsigned int vHigh;
+
+    /*
+     * unsigned int vWide - szerokosc planszy
+     */
+    unsigned int vWide;
 
     /********** PRIVATE: END **********/
 };
