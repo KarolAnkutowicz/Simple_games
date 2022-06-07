3bb2f8b64a604c0e7eeade73960920fbf4f5f6f4
Karol Ankutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2022-06-07 11:58:51 +0200

    Add cPrinting
    Add new class - cPrinting.

---
+++ Sokoban/main/cprinting.cpp
@@ -0,0 +1,14 @@
+/********** BEGIN_OF_FILE **********/
+/*
+ * author: Karol Ankutowicz
+ * file: main.cpp
+ */
+
+#include "cprinting.hpp"
+
+ /********** PUBLIC: BEGIN **********/
+
+ /********** PUBLIC: END **********/
+
+ /* main.cpp */
+ /********** END_OF_FILE **********/
---
+++ Sokoban/main/cprinting.hpp
@@ -0,0 +1,23 @@
+/********** BEGIN_OF_FILE **********/
+/*
+ * author: Karol Ankutowicz
+ * file: main.cpp
+ */
+
+#pragma once
+
+class cPrinting
+{
+    /********** PUBLIC: BEGIN **********/
+public:
+
+    /********** PUBLIC: END **********/
+    
+    /********** PRIVATE: BEGIN **********/
+private:
+
+    /********** PRIVATE: BEGIN **********/
+};
+
+/* main.cpp */
+/********** END_OF_FILE **********/
