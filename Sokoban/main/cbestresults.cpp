949ba69863ca07147454cb57195da2dc7e596d55
Karol Ankutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2022-08-17 23:37:01 +0200

    Add cBestResults
    Add new class - cBestResults which modelling lists of best results with times or number of moves.

---
+++ Sokoban/main/cbestresults.cpp
@@ -0,0 +1,10 @@
+/********** BEGIN_OF_FILE **********/
+/*
+ * author: Karol Ankutowicz
+ * file: cbestresults.cpp
+ */
+
+#include "cbestresults.hpp"
+
+ /* cbestresults.cpp */
+ /********** END_OF_FILE **********/
---
+++ Sokoban/main/cbestresults.hpp
@@ -0,0 +1,39 @@
+/********** BEGIN_OF_FILE **********/
+/*
+ * author: Karol Ankutowicz
+ * file: cbestresults.hpp
+ */
+
+#pragma once
+
+#include <vector>
+#include "csymbols.hpp"
+
+typedef unsigned int timeSeconds;
+typedef unsigned int moveNumber;
+const unsigned int maxResults = 20;
+
+class cBestResults
+{
+	/********** PUBLIC: BEGIN **********/
+
+public:
+
+	void mAddRecordMove(numberLevel aLevel, timeSeconds aResult);
+	void mAddRecordTime(numberLevel aLevel, moveNumber aResult);
+
+	/********** PUBLIC: END **********/
+
+	/********** PRIVATE: BEGIN **********/
+
+private:
+
+	std::vector<std::vector<moveNumber> > vBestMoveNumber;
+	std::vector<std::vector<timeSeconds> > vBestTimes;
+
+	/********** PRIVATE: END **********/
+
+};
+
+/* cbestresults.hpp */
+/********** END_OF_FILE **********/
